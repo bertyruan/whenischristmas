49bb45ff126a4f3734a828c32fa29228a9ee2bca
Berty Qiming Ruan <berty.ruan@gmail.com>
2013-11-26 23:48:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whenischristmas::Application.config.secret_key_base = '4e17391e612ad42110d7dec09ca56b324148192ac4dcc8ece2c1cb9e10b5f0ffb450260927aef42f0fca925a4b4a92b75f8a1c2859c21580a000ca75c673e763'
